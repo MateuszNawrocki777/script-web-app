71c9cb16d68f2dce72526e5612edbe99c766e8ac
Mateusz Nawrocki <01187259@pw.edu.pl>
2025-07-31 18:10:46 +0200

    Make enter trigger log in on log in page

--- frontend/src/components/LoginTab/LoginTab.jsx
+++ frontend/src/components/LoginTab/LoginTab.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import logInRequest from "../../services/api/requests/login";
 
@@ -10,9 +10,16 @@ let username = "", password = "";
 
 export default function LoginTab({ setLoggedIn }) {
     const [errorMessage, setErrorMessage] = useState("");
+    const buttonRef = useRef(null);
 
     async function handleLogin() {
         await logInRequest(username, password, setErrorMessage, setLoggedIn);
+    }
+
+    function handleEnter(event) {
+        if (event.key === "Enter") {
+            buttonRef.current?.click();
+        }
     }
 
     return (
@@ -21,11 +28,13 @@ export default function LoginTab({ setLoggedIn }) {
                 Log in
             </h1>
             <input className="login-button" type="text" id="username" placeholder="Username"
-             onChange={(e) => {username = e.target.value}} />
+             onChange={(e) => {username = e.target.value}}
+             onKeyDown={handleEnter} />
             <input className="login-button" type="password" id="password" placeholder="Password"
-             onChange={(e) => {password = e.target.value}} />
+             onChange={(e) => {password = e.target.value}}
+             onKeyDown={handleEnter} />
             <LoadingButton className="login-button" id="login-button"
-             onClick={handleLogin}>
+             onClick={handleLogin} ref={buttonRef}>
                 Log in
             </LoadingButton>
             <p className="login-error-message">{errorMessage}</p>
